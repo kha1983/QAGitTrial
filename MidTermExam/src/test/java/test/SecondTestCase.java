625d8cb7573ddf20c6c0273ae85d0a24657fd398
khadak <khadak.kathayat100@gmail.com>
2020-02-21 12:12:06 -0600

    Secondtest Commit

--- MidTermExam/src/test/java/test/SecondTestCase.java
+++ MidTermExam/src/test/java/test/SecondTestCase.java
@@ -1,5 +1,31 @@
 package test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import page.LoginPage;
+import util.BrowserFactory;
+
 public class SecondTestCase {
+	
+	WebDriver driver;
+	@Test
+	public void ValidUserShoulBeAbleToLogIn()     {
+		
+		driver=BrowserFactory.startBrowser();
+		
+		LoginPage loginpage=PageFactory.initElements(driver, LoginPage.class);
+		
+		loginpage.login();
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.close();
+		
+		driver.quit();
+	}
+
+
 
 }
